9f5613ad4f0b6b0c7a9ac5826f6e9ca7dc6db9ab
Alex <fedoseev_av91@mail.ru>
2023-06-08 23:10:55 +0300

    Test

---
+++ FrendsAndDog/Program.cs
@@ -0,0 +1,17 @@
+﻿int distance = 10000;
+int count = 0;
+int firstFriendSpeed = 1;
+int secondFriendSpedd = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while(distanse > 10)
+{
+    if(friend == 1)
+    {
+        
+    }
+    else 
+
+
+}
