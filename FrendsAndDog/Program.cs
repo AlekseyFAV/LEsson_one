d1079fa662f3ed22473caabdaaf3090952a8ba6e
Alex <fedoseev_av91@mail.ru>
2023-06-09 14:15:28 +0300

    proba reshenia

--- FrendsAndDog/Program.cs
+++ FrendsAndDog/Program.cs
@@ -1,17 +1,26 @@
-﻿int distance = 10000;
+﻿using System;
+
+int distance = 10000;
 int count = 0;
 int firstFriendSpeed = 1;
 int secondFriendSpedd = 2;
 int dogSpeed = 5;
 int friend = 2;
+int time = 0;
 
-while(distanse > 10)
+while (distance > 10)
 {
-    if(friend == 1)
+    if (friend == 1)
     {
-        
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
     }
-    else 
-
-
+    else
+    {
+        time = distance / (secondFriendSpedd + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpedd) * time;
+count = count + 1;
 }
+Console.Write(count);
